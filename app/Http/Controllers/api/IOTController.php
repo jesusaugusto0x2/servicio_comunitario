613a3101391ca5bf7837441f149b411d74e98812
kvnsnchz <kvn9616@gmail.com>
2020-06-03 00:25:56 +0200

    agregando

--- app/Http/Controllers/api/IOTController.php
+++ app/Http/Controllers/api/IOTController.php
@@ -11,12 +11,43 @@ class IOTController extends Controller
         $client = new Client(['base_uri' => 'http://[2001:660:5307:3116::a482]']);
 
         $response = $client->request('GET', "temperature");
+        $data = $response->getBody()->getContents();
 
-        dd($response->getBody()->getContents());
-        $data = json_decode($response->getBody());
-        dd($data);
         return response()->json([
-            'temperature' => 10,
+            'temperature' => $data,
+        ]);
+    }
+
+    public function humidity (Request $request) {
+        $client = new Client(['base_uri' => 'http://[2001:660:5307:3116::a482]']);
+
+        $response = $client->request('GET', "humidity");
+        $data = $response->getBody()->getContents();
+
+        return response()->json([
+            'humidity' => $data,
+        ]);
+    }
+
+    public function temperature_threshold (Request $request) {
+        $client = new Client(['base_uri' => 'http://[2001:660:5307:3116::a482]']);
+
+        $response = $client->request('GET', "temp_thr");
+        $data = $response->getBody()->getContents();
+
+        return response()->json([
+            'temperature_threshold' => $data,
+        ]);
+    }
+
+    public function humidity_threshold (Request $request) {
+        $client = new Client(['base_uri' => 'http://[2001:660:5307:3116::a482]']);
+
+        $response = $client->request('GET', "humi_thr");
+        $data = $response->getBody()->getContents();
+
+        return response()->json([
+            'humidity_threshold' => $data,
         ]);
     }
 }
