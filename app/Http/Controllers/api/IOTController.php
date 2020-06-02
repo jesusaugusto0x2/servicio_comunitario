1c127b67e5d69f1697f79baf962f0ee7e5cfecda
kvnsnchz <kvn9616@gmail.com>
2020-06-02 23:35:43 +0200

    agregando

--- app/Http/Controllers/api/IOTController.php
+++ app/Http/Controllers/api/IOTController.php
@@ -3,11 +3,13 @@ namespace App\Http\Controllers\api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use GuzzleHttp\Client;
+use \GuzzleHttp\Client;
 
 class IOTController extends Controller
 {
     public function temperature (Request $request) {
+        $client = new Client(['base_uri' => 'http://[2001:660:5307:3116::a482]']);
+
         $response = $client->request('GET', "temperature");
         $data = json_decode($response->getBody());
         dd($data);
